1d1174abe0ffed182ffd8f51078db866f0a9ee37
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-11 16:38:51 -0500

    Started homework part 2

---
+++ homework/homework_part_2/JS/app.js
@@ -0,0 +1,26 @@
+console.log("hello");
+
+// What is the difference between interpolation and concatenation? Give an example of each.
+// -- concatenation is when you use + to combine strings together. interpolation is when you use ${string} to combine strings.
+//------------------------------------------------------------//
+// What does the acronym DRY stand for? Why should we pay attention to it? What programming tools have we learned to write DRY code?
+//  -- DRY = DON'T REPEAT YOURSELF
+//------------------------------------------------------------//
+// What is the difference between declaring a variable and assigning a value to a variable? What do we mean when we say "define" a variable?
+// -- declaring a variable is giving the variable a name. Assigning a value to a variable is when you give a variable a; number, string or boolean.
+//------------------------------------------------------------//
+// When should we use const and when should we use let?
+
+//------------------------------------------------------------//
+// Unix question: What is a "parent" directory?
+
+// -- it's the directory above the current directory you're working in.
+//------------------------------------------------------------//
+// Unix, again: If you're not sure about how to use a Unix command, how can you get more info without being connected to the internet?
+
+//------------------------------------------------------------//
+// More Unix: What is "tab completion" and why is it aweseome?
+
+// -- tab completion is when you're writing code and you hit TAB and it autofinishes what it thinks you wanted to write out
+
+//------------------------------------------------------------//
